6d4dadb9593a289be920cc4d171d7cdfca3464d0
Katherine Martinez <kmartinezmedia@gmail.com>
2014-08-28 19:03:41 -0400

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,130 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'auth0'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider, authProvider, $httpProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+    // This is the Login state
+    .state('login', {
+      url: "/login",
+      templateUrl: "templates/login.html",
+      controller: "LoginCtrl"
+    })
+
+    // setup an abstract state for the tabs directive
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "templates/menu.html",
+      controller: 'AppCtrl',
+      data: {
+        // This tells Auth0 that this state requires the user to be logged in.
+        // If the user isn't logged in and he tries to access this state
+        // he'll be redirected to the login page
+        requiresLogin: true
+      }
+    })
+
+    // Your app states
+    .state('app.dash', {
+      url: '/dashboard',
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/dashboard.html",
+          controller: 'DashCtrl'
+        }
+      }
+    })
+
+    .state('app.search', {
+      url: "/search",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/search.html"
+        }
+      }
+    })
+
+    .state('app.browse', {
+      url: "/browse",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/browse.html"
+        }
+      }
+    })
+
+    .state('app.profile', {
+      url: "/profile",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/profile.html",
+          controller: 'ProfileCtrl'
+        }
+      }
+    })
+
+    .state('app.playlists', {
+      url: "/playlists",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/playlists.html",
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+    .state('app.single', {
+      url: "/playlists/:playlistId",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/playlist.html",
+          controller: 'PlaylistCtrl'
+        }
+      }
+    });
+
+  // Configure Auth0
+  authProvider.init({
+   domain: 'youthconnect.auth0.com',
+   clientID: 'GWdPLDnNOoJAcFTMtjlpLb57y4ui5hNL',
+   callbackURL: location.href,
+   // This is the name of the state to redirect to if the user tries to enter
+   // to a restricted page
+   loginState: 'login'
+  });
+
+  $httpProvider.interceptors.push('authInterceptor');
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
+
+})
+
+.run(function(auth) {
+  // This hooks al auth events to check everything as soon as the app starts
+  auth.hookEvents();
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,51 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, auth, $state) {
+  $scope.auth = auth;
+  $scope.logout = function() {
+    auth.signout();
+    $state.go('login');
+  }
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+
+
+.controller('DashCtrl', function($scope, auth, $state) {
+  $scope.auth = auth;
+
+})
+
+.controller('ProfileCtrl', function($scope, auth, $state) {
+})
+
+.controller('LoginCtrl', function($scope, auth, $state) {
+  auth.signin({
+    // This is a must for mobile projects
+    popup: true,
+    // Make the widget non closeable
+    standalone: true,
+    // This asks for the refresh token
+    // So that the user never has to log in again
+    offline_mode: true,
+    device: 'Phone'
+  }, function() {
+    // Login was successful
+    $state.go('app.dash');
+  }, function(error) {
+    // Oops something went wrong during login:
+    console.log("There was an error logging in", error);
+  });
+});
